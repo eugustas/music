35dc4548d829e33e4f43f004184bcbf8d4e003fe
eugustas <gustasuport@gmail.com>
2025-03-14 11:27:37 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
+<!-- 
+  Copyright (c) 2025 Gustavo Business. Todos os direitos reservados.
+
+  Este código é fornecido sob a Licença MIT. Você pode usar, copiar, modificar, mesclar, publicar, distribuir, sublicenciar e/ou vender cópias deste software, conforme permitido pela licença. Consulte o arquivo LICENSE para mais detalhes.
+
+  Este software é fornecido "no estado em que se encontra", sem qualquer garantia expressa ou implícita, incluindo, mas não se limitando a, garantias de comercialização ou adequação a um propósito específico. O autor ou os detentores do copyright não se responsabilizam por quaisquer danos decorrentes do uso do software.
+-->
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('gustavo-songs-cache').then((cache) => {
